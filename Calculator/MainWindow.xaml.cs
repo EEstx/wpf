0617964d2286ddcabea2d961b6526cd1de388612
EEstx <vovavas1234@mail.ru>
2026-03-24 22:33:08 +0300

    calc

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -16,8 +16,81 @@ namespace Calculator;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private double _lastNumber, _result;
+    private string _selectedOperator = "";
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    // Обработка нажатия на цифровую кнопку
+    private void Digit_Click(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)sender;
+        
+        if (Display.Text == "0" || _selectedOperator == "=")
+        {
+            Display.Text = button.Content.ToString();
+            
+            if (_selectedOperator == "=") _selectedOperator = "";
+        }
+        else
+        {
+            Display.Text += button.Content.ToString();
+        }
+    }
+
+    // Выбор математической операции (+, -, *, /)
+    private void Operation_Click(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)sender;
+        
+        if (double.TryParse(Display.Text, out _lastNumber))
+        {
+            Display.Text = "0";
+            _selectedOperator = button.Content.ToString();
+        }
+    }
+
+    // Кнопка вычисления результата (равно)
+    private void Equals_Click(object sender, RoutedEventArgs e)
+    {
+        double newNumber;
+        
+        if (double.TryParse(Display.Text, out newNumber))
+        {
+            switch (_selectedOperator)
+            {
+                case "+":
+                    _result = _lastNumber + newNumber;
+                    break;
+                case "-":
+                    _result = _lastNumber - newNumber;
+                    break;
+                case "*":
+                    _result = _lastNumber * newNumber;
+                    break;
+                case "/":
+                    _result = newNumber == 0 ? 0 : _lastNumber / newNumber;
+                    break;
+                default:
+                    _result = newNumber;
+                    break;
+            }
+            Display.Text = _result.ToString();
+            
+            _selectedOperator = "=";
+        }
+    }
+
+    // Сброс калькулятора (кнопка С)
+    private void Clear_Click(object sender, RoutedEventArgs e)
+    {
+        _lastNumber = 0;
+        _result = 0;
+        _selectedOperator = "";
+        
+        Display.Text = "0";
     }
 }
